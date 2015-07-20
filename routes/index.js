a17ba5c00a85c1baa3c0e1409c4ff47b32b3d8e8
Diego Plascencia <diego-d@outlook.com>
2015-07-20 00:23:32 -0500

    questions list and autoload MW

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller.js');
+var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+// Página de entrada (home page)
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Quiz' });
 });
 
 router.get('/author', function(req, res){
     res.render('author', {title: 'Creditos'});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.param('quizId', quizController.load);
 
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
